050c7a98d8de1f204082fc38ae21930721bbf377
Ravi Ranjan <ravi2519@gmail.com>
2020-02-28 03:44:16 -0800

    Implemented Configuration annotation

---
+++ src/main/java/in/ravir/jokesapp/config/ChuckConfiguration.java
@@ -0,0 +1,19 @@
+package in.ravir.jokesapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+/**
+ * ChuckConfiguration
+ */
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+    
+}
